871a4d875a8f2557332dcf068a062b633dfbd629
Aluno_DEV <lucas.fonseca.sesisenaisp@gmail.com>
2024-06-04 13:54:43 -0300

    c

--- app/src/main/java/com/example/bluetoothapp/MainActivity.java
+++ app/src/main/java/com/example/bluetoothapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.bluetoothapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +17,27 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String[] PERMISSIONS_STORAGE = {
+            android.Manifest.permission.BLUETOOTH_SCAN,
+            android.Manifest.permission.BLUETOOTH_CONNECT,
+            android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+            android.Manifest.permission.BLUETOOTH_ADMIN,
+            android.Manifest.permission.BLUETOOTH_ADVERTISE
+    };
+    private static String[] PERMISSIONS_LOCATION = {
+            android.Manifest.permission.BLUETOOTH_SCAN,
+            android.Manifest.permission.BLUETOOTH_CONNECT,
+            android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+            android.Manifest.permission.BLUETOOTH_ADMIN,
+            android.Manifest.permission.BLUETOOTH_ADVERTISE
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermissions();
 
         Button scanButton = findViewById(R.id.button_scan_qr);
         scanButton.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +60,46 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "Leitura cancelada", Toast.LENGTH_LONG).show();
             }
+        }
+    }
+
+    private void checkPermissions(){
+        int permission1 = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT);
+        int permission2 = ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN);
+        int permission3 = ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN);
+        int permission4 = ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_PRIVILEGED);
+        int permission5 = ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE);
+        if (permission1 != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    1
+            );
+        } else if (permission2 != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_LOCATION,
+                    1
+            );
+        } else if (permission3 != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_LOCATION,
+                    1
+            );
+        }else if (permission4 != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_LOCATION,
+                    1
+            );
+        }else if (permission5 != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_LOCATION,
+                    1
+            );
         }
     }
 }
